74c2f31b2af1da4738194e54fb0607f86c1cc6ee
Michael Chapa <michaelchapa1@gmail.com>
2020-08-24 22:21:39 -0700

    Added getMean, getMidRange

--- normalize.py
+++ normalize.py
@@ -1,1 +1,18 @@
+import pandas as pd
+import numpy as np
 
+# A)
+# 1. Calculate Mean & Midrange
+def getMean(data):
+    rows, _ = data.shape
+    data = data[data.columns[3:]].sum(axis = 0)
+    data = data / rows
+    return data
+
+# def getMidRange(data):
+    
+    
+    
+data = pd.read_csv('https://raw.githubusercontent.com/michaelchapa/dataMining_data_normalization/master/hwk01.csv')
+series = getMean(data)
+print(series)
