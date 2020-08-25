4c298b78afd3a2d32953d4fb997838e84da114e0
Michael Chapa <michaelchapa1@gmail.com>
2020-08-25 16:03:30 -0700

    completed: getMidRange(), began: getMode()

--- normalize.py
+++ normalize.py
@@ -1,18 +1,34 @@
 import pandas as pd
 import numpy as np
 
-# A)
-# 1. Calculate Mean & Midrange
 def getMean(data):
     rows, _ = data.shape
     data = data[data.columns[3:]].sum(axis = 0)
     data = data / rows
-    return data
+    print(data)
 
-# def getMidRange(data):
+def getMidRange(data):
+    rows, _ = data.shape
+    midranges = []
+    
+    data.sort_values(by='C', inplace = True)
+    midranges.append(data.loc[(rows // 2), 'C'])
+    
+    data.sort_values(by='D', inplace = True)
+    midranges.append(data.loc[(rows // 2), 'D'])
+    
+    data.sort_values(by='E', inplace = True)
+    midranges.append(data.loc[(rows // 2), 'E'])
+    
+    data.sort_values(by='F', inplace = True)
+    midranges.append(data.loc[(rows // 2), 'F'])
+    
+    print(midranges)
+    
+# def getMode(data):
     
     
     
 data = pd.read_csv('https://raw.githubusercontent.com/michaelchapa/dataMining_data_normalization/master/hwk01.csv')
-series = getMean(data)
-print(series)
+getMean(data)
+getMidRange(data)
